818494a991161dae158e49de139b05252fbbd3ef
avan16 <134949553+avan16@users.noreply.github.com>
2023-12-25 17:03:50 +0500

    Initial Commit

---
+++ Vilage/VilageApp.swift
@@ -0,0 +1,17 @@
+//
+//  VilageApp.swift
+//  Vilage
+//
+//  Created by Avantika on 2023-12-25.
+//
+
+import SwiftUI
+
+@main
+struct VilageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
